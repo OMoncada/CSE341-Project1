312e8798d6e36fc7af4b4b5d10e0b08999f3db40
O'Moncada <oscar.moncada702@gmail.com>
2025-05-10 00:56:07 -0400

    Agregar al repositorio new

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => { res.send('Hello World');});
-
-router.use('/users', require('./users'));
+router.get('/', (req, res) => { res.send('Hello World')});
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-
 const mongodb = require('./data/database');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-
-
 mongodb.initDb((err) => {
-if(err) {
-console.log(err);
-}
-else {
-app.listen(port, () => {console.log(`Database is listening and node Running on port $${(port)}`)});
-}
+  if(err) {
+    console.log(err);
+  }
+  else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node running on port ${port}`)
+    })
+  }
+})
+
+app.listen(port, () => {
+  console.log('Web Server is listening at port ' + (port));
 });
